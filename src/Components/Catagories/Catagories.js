420bbeffbb85783ff9e92b95c28fb0c5e89d9af7
shafik720 <shafikrasel5@gmail.com>
2023-01-10 23:42:46 +0600

    working on catagory

--- src/Components/Catagories/Catagories.js
+++ src/Components/Catagories/Catagories.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import workImg from '../../Utilities/Img/suitcase.png';
+import './Catagories.css';
 
 const Catagories = () => {
     return (
-        <div>
-            <h2>Catagory here</h2>
+        <div className="catagory-div">
+            <h3 className="">Catagories</h3>
+            <div className="catagory">
+                <div className="catagory-single">
+                    <span><img src={workImg} alt="" /></span>
+                    <p>Work</p>
+                </div>
+            </div>
         </div>
     );
 };
