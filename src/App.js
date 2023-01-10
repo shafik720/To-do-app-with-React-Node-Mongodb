3ecbd16141305a72c16c14cf756bf1782c286362
shafik720 <shafikrasel5@gmail.com>
2023-01-10 23:22:54 +0600

    working on catagory

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
+import Catagories from './Components/Catagories/Catagories';
 
 function App() {
   return (
     <div className="main-div">
       <Header></Header>
       <Banner></Banner>
+      <Catagories></Catagories>
     </div>
   );
 }
---
+++ src/Components/Catagories/Catagories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Catagories = () => {
+    return (
+        <div>
+            <h2>Catagory here</h2>
+        </div>
+    );
+};
+
+export default Catagories;
