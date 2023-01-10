bd77f1cbb5ae5e2e4de7a7fdf1224ef58014a5b8
shafik720 <shafikrasel5@gmail.com>
2023-01-10 21:42:52 +0600

    working on header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className='header-div'>
+            <div className="left-part">
+                <button><FontAwesomeIcon icon={faBars} />
+                </button>
+            </div>
+            <div className="right-part"></div>
         </div>
     );
 };
