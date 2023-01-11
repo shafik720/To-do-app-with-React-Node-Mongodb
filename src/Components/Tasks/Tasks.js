39a2c8d3f2520b8f44af6f234030799c67378593
shafik720 <shafikrasel5@gmail.com>
2023-01-11 10:14:42 +0600

    working on tasks section

--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Tasks.css';
 
 const Tasks = () => {
     return (
-        <div>
-            <h2>tasks here</h2>
+        <div className="task-div">
+            <h3>You have 10 tasks to complete</h3>
+            <div className="all-task">
+                <div className="single-task">
+                    <div className="single-task-left">
+                        <input type="radio" name="" id="" />
+                        <p>Coding for 6 hours</p>
+                    </div>
+                    <div className="single-task-right">
+                        <p>9 am</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
