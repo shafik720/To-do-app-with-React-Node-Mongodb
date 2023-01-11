26b0f4b1e2d635b7efc32604422c269a7bcd57ee
shafik720 <shafikrasel5@gmail.com>
2023-01-11 10:46:49 +0600

    working on bottom section

--- src/Components/BottomSection/BottomSection.js
+++ src/Components/BottomSection/BottomSection.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './BottomSection.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faCalendar, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 const BottomSection = () => {
     return (
-        <div>
-            <h2>Bottom here</h2>
+        <div className="bottom-div">
+            <div className="bottom-left">
+                <span><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
+                <span><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
+            </div>
+            <div className="bottom-mid">
+                <span><FontAwesomeIcon className="add-icon" icon={faPlusCircle} /> </span>
+            </div>
+            <div className="bottom-right">
+                <span><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
+                <span><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
+            </div>
         </div>
     );
 };
