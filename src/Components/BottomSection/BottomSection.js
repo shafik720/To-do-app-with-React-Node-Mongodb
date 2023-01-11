ef6fbf01c35b6e1de7832de23415670c5dce9024
shafik720 <shafikrasel5@gmail.com>
2023-01-11 11:51:34 +0600

    working on popup

--- src/Components/BottomSection/BottomSection.js
+++ src/Components/BottomSection/BottomSection.js
@@ -5,13 +5,18 @@ import { faHouse, faCalendar, faPlusCircle, faGear, faClipboard } from '@fortawe
 
 
 const BottomSection = () => {
+    function addTask() {
+        console.log('clicked');
+    }
     return (
-        <div className="bottom-div">
-            <span draggable><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
-            <span draggable><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
-            <span draggable><FontAwesomeIcon className="add-icon" icon={faPlusCircle} /> </span>
-            <span draggable><FontAwesomeIcon className="single-icon" icon={faClipboard} /> </span>
-            <span draggable><FontAwesomeIcon className="single-icon" icon={faGear} /> </span>
+        <div className="">
+            <div className="bottom-div">
+                <span draggable><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
+                <span draggable><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
+                <span onClick={addTask} draggable><FontAwesomeIcon className="add-icon" icon={faPlusCircle} /> </span>
+                <span draggable><FontAwesomeIcon className="single-icon" icon={faClipboard} /> </span>
+                <span draggable><FontAwesomeIcon className="single-icon" icon={faGear} /> </span>
+            </div>
         </div>
     );
 };
