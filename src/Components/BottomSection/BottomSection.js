62f980aead674bc77218aa7f4f0cc343c703b7bb
shafik720 <shafikrasel5@gmail.com>
2023-01-11 10:50:14 +0600

    working on bottom section

--- src/Components/BottomSection/BottomSection.js
+++ src/Components/BottomSection/BottomSection.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './BottomSection.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faCalendar, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faCalendar, faPlusCircle, faGear, faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 
 const BottomSection = () => {
     return (
         <div className="bottom-div">
-            <div className="bottom-left">
-                <span><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
-                <span><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
-            </div>
-            <div className="bottom-mid">
-                <span><FontAwesomeIcon className="add-icon" icon={faPlusCircle} /> </span>
-            </div>
-            <div className="bottom-right">
-                <span><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
-                <span><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
-            </div>
+            <span><FontAwesomeIcon className="single-icon" icon={faHouse} /> </span>
+            <span><FontAwesomeIcon className="single-icon" icon={faCalendar} /> </span>
+            <span><FontAwesomeIcon className="add-icon" icon={faPlusCircle} /> </span>
+            <span><FontAwesomeIcon className="single-icon" icon={faClipboard} /> </span>
+            <span><FontAwesomeIcon className="single-icon" icon={faGear} /> </span>
         </div>
     );
 };
