60db14528d423fa3827af806ffc3acc53816bf27
shafik720 <shafikrasel5@gmail.com>
2023-01-11 14:42:32 +0600

    adding to mongodb

--- src/Components/BottomSection/BottomSection.js
+++ src/Components/BottomSection/BottomSection.js
@@ -11,6 +11,7 @@ const BottomSection = () => {
     // react hook form functionality
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
+        //---------- adding data to mongodb
         fetch('http://localhost:5000/addTask',{
             method:'POST',
             headers : {
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './Tasks.css';
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/alltasks')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div className="task-div">
             <h3>You have 10 tasks to complete</h3>
