ada3aa97105e5c8d86d33d0fd8df498c6c9ff987
shafik720 <shafikrasel5@gmail.com>
2023-01-11 10:02:58 +0600

    working on tasks section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Banner from './Components/Banner/Banner';
 import Catagories from './Components/Catagories/Catagories';
+import Tasks from './Components/Tasks/Tasks';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Banner></Banner>
       <Catagories></Catagories>
+      <Tasks></Tasks>
     </div>
   );
 }
---
+++ src/Components/Tasks/Tasks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tasks = () => {
+    return (
+        <div>
+            <h2>tasks here</h2>
+        </div>
+    );
+};
+
+export default Tasks;
