38c81ae85691d4230a93e3eacb8f9d6d5b89bd58
shafik720 <shafikrasel5@gmail.com>
2023-01-11 14:49:42 +0600

    Loading from  mongodb

--- src/Components/Tasks/SingleTasks/SingleTasks.js
+++ src/Components/Tasks/SingleTasks/SingleTasks.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const SingleTasks = () => {
+const SingleTasks = (props) => {
+    const{taskName, startTime} = props.index;
     return (
         <div>
             <div className="single-task">
                     <div className="single-task-left">
                         <input type="radio" name="" id="" />
-                        <p>Coding for 6 hours</p>
+                        <p>{taskName}</p>
                     </div>
                     <div className="single-task-right">
-                        <p>9 am</p>
+                        <p>{startTime}</p>
                     </div>
                 </div>
         </div>
