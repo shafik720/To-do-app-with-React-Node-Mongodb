82c3ba2be1c76db14a5fb6e922fda3bd5479af08
shafik720 <shafikrasel5@gmail.com>
2023-01-13 18:51:20 +0600

    editing task on mongodb

--- src/Components/Tasks/EditTask/EditTask.js
+++ src/Components/Tasks/EditTask/EditTask.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,27 +7,36 @@ import { faMultiply } from '@fortawesome/free-solid-svg-icons';
 const EditTask = () => {
     const { id } = useParams();
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+    };
     const navigate = useNavigate();
 
-    document.querySelector('.main-div').classList.add('active');
+ 
     
+    // this will take the user to home page when the pop model will be closed
     function backToApp(){
         navigate(`/`);
         
         document.querySelector('.main-div').classList.remove('active');
     }
+
+    let[singleTask, setSingleTask] = useState([]);
+    useEffect(()=>{
+        fetch(`http://localhost:5000/alltasks/${id}`)
+        .then(res=>res.json())
+        .then(data=>setSingleTask(data))
+    },[])
     return (
         <div>
-            {/* <h2 className="text-danger">Edit your task here : {id} </h2> */}
             <div className="popup-parent active">
                 <div className="popup-model">
-                    <h2 className='text-center my-4 fw-bold'>Edit your task here : {id}</h2>
+                    <h2 className='text-center my-4 fw-bold'>Edit your task </h2>
                     <form className='submit-form' >
                         <p className='label-for-task'>Task Name</p>
-                        <input {...register("taskName", { required: true, maxLength: 20 })} />
+                        <input value={singleTask.taskName} {...register("taskName", { required: true, maxLength: 20 })} />
                         <p className='label-for-task'>Start Time</p>
-                        <input type="time" {...register("startTime", { min: 18, max: 99 })} />
+                        <input value={singleTask.startTime} type="time" {...register("startTime", { min: 18, max: 99 })} />
                         <input className='submit-task-button' type="submit" />
                     </form>
                     <span onClick={backToApp} className='popup-close-button'><FontAwesomeIcon className="" icon={faMultiply} /></span>
